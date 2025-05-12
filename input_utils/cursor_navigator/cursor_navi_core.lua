109e8adc779d5fc5990d39d3cc5567287499500a
Dean <kouteki@Deans-MBAir.local>
2025-05-12 20:38:22 +0900

    build and verify naviCanvas nudge feature

--- input_utils/cursor_navigator/cursor_navi_core.lua
+++ input_utils/cursor_navigator/cursor_navi_core.lua
@@ -8,10 +8,11 @@ local NaviCanvas = require("input_utils.cursor_navigator.navi_assist_canvas")
 local cursor_navigator = {}
 local modal = hs.hotkey.modal.new({"alt"}, "c")
 
--- depth
+-- depth & step
 local currentDepth = 0
 local maxDepth = 4
--- keys
+local nudgeStep = 2
+-- key maps
 local directionKeys = {"u","i","o",
                        "j",    "l",
                        "h","k","_"}
@@ -20,40 +21,49 @@ local keyToAnchorMap = {
     j = {r = 3, c = 2},                     l = {r = 3, c = 4},
     h = {r = 4, c = 2}, k = {r = 4, c = 3}, ["_"] = {r = 4, c = 4}
 }
+local nudgeKeyMap = {
+    up   = { x = 0, y = -nudgeStep }, down  = { x = 0, y =  nudgeStep },
+    left = { x = -nudgeStep, y = 0 }, right = { x =  nudgeStep, y = 0 },
+    ["ctrl-i"]= { x = 0, y = -nudgeStep }, ["ctrl-k"]= { x = 0, y =  nudgeStep },
+    ["ctrl-j"]= { x = -nudgeStep, y = 0 }, ["ctrl-l"]= { x =  nudgeStep, y = 0 }
+}
 -- screen & canvas
 local screenFrame = {x = 0, y = 0, w = 0, h = 0}
 local currentRect = {x = 0, y = 0, w = 0, h = 0}
 -- vector2 pointer
-local currentPointer = {x = 0, y = 0}
+local currentPointer     = {x = 0, y = 0}
 local lastClickedPointer = {x = 0, y = 0}
 -- UI
-local crosshair = Crosshair:new()
+local crosshair  = Crosshair:new()
 local naviCanvas = NaviCanvas:new()
 
 local function clickPointer(currentPointer)
     local point = currentPointer
     hs.mouse.absolutePosition(point)
-
+    --
     local clickDown = hs.eventtap.event.newMouseEvent(
-        hs.eventtap.event.types.leftMouseDown, { x = point.x, y = point.y }
-    )
+        hs.eventtap.event.types.leftMouseDown,
+        { x = point.x, y = point.y })
     local clickUp = hs.eventtap.event.newMouseEvent(
-        hs.eventtap.event.types.leftMouseUp, { x = point.x, y = point.y }
-    )
+        hs.eventtap.event.types.leftMouseUp,
+        { x = point.x, y = point.y })
+    --
     clickDown:post()
     hs.timer.usleep(20000)
     clickUp:post()
+    --
     lastClickedPointer = point
 end
 
-local function nudgeNaviCanvas()
-    local vec2 = { x = 2, y = 2}
-    local targetPoint = { x = currentPointer.x + vec2.x, y = currentPointer.y + vec2.y}
-    currentRect.x = currentRect.x + vec2.x
-    currentRect.y = currentRect.y + vec2.y
+local function nudgeNaviCanvas(dx, dy)
+    local targetPoint = { x = currentPointer.x + dx, y = currentPointer.y + dy }
+    currentRect.x = currentRect.x + dx
+    currentRect.y = currentRect.y + dy
+    --
     crosshair:moveTo(targetPoint.x, targetPoint.y)
     naviCanvas:refineMask(currentRect)
     naviCanvas:drawAssistCanvas(currentRect, currentDepth-1)
+    --
     currentPointer = targetPoint
 end
 
@@ -83,18 +93,39 @@ local function handleAnchorKey(key)
     end
 end
 
+local function buildKeyId(event)
+    local kc = event:getKeyCode()
+    local key = hs.keycodes.map[kc] or ""
+    local flags = event:getFlags()
+    local mods = {}
+    if flags.ctrl  then table.insert(mods, "ctrl")  end
+    if flags.cmd   then table.insert(mods, "cmd")   end
+    if flags.alt   then table.insert(mods, "alt")   end
+    if flags.shift then table.insert(mods, "shift") end
+    local prefix = table.concat(mods, "-")
+    return prefix ~= "" and (prefix .. "-" .. key) or key
+end
+
 local function navigatorHandler(event)
+    --
+    local index     = buildKeyId(event)
+    local nudgeVec2 = nudgeKeyMap[index]
+    if nudgeVec2 and currentDepth > 0 then
+        nudgeNaviCanvas(nudgeVec2.x, nudgeVec2.y)
+        return true
+    end
+    --
     local key = event:getCharacters(true)
     if hs.fnutils.contains(directionKeys, key) then
         handleAnchorKey(key)
         return true
     end
     return false
-    end
+end
 
 local function init_navigator()
     currentDepth = 0
-    screenFrame = hs.screen.mainScreen():fullFrame()
+    screenFrame  = hs.screen.mainScreen():fullFrame()
     currentPointer = {
         x = screenFrame.x + screenFrame.w / 2,
         y = screenFrame.y + screenFrame.h / 2,
@@ -116,10 +147,6 @@ end
 modal:bind({}, ".", function()
     clickPointer(lastClickedPointer)
     modal:exit()
-end)
-
-modal:bind({}, "right", function()
-    nudgeNaviCanvas()
 end)
 
 function modal:exited()
